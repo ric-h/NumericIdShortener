1844da5a58dc1250f3f4616e8188010f51d7832a
Richard H <richard.lima@gmail.com>
2023-02-14 12:16:00 -0300

    Update Shortener.js
    a comment

--- Shortener.js
+++ Shortener.js
@@ -2,7 +2,7 @@ class IDShortener {
   constructor() {
     this.ID = null;
     this.Code = null;
-    this.__word = " aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTwWuUxXyYzZ123456789";
+    this.__word = " aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTwWuUxXyYzZ123456789"; //this string can be scrambled to give different results
   }
   
   shortenID(id) {
